ec4dd8af78d38f7585220296f487ae39308d2603
recca0120 <recca0120@gmail.com>
2018-06-22 15:28:26 +0800

    完成新增功能

--- src/Todolist.php
+++ src/Todolist.php
@@ -4,4 +4,15 @@ namespace Recca0120\Todolist;
 
 class Todolist
 {
+    private $items = [];
+
+    public function all()
+    {
+        return $this->items;
+    }
+
+    public function add($text)
+    {
+        array_push($this->items, $text);
+    }
 }
