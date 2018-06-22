36fae8f8fd6b86ec49d502639fa5f4700abd45ee
recca0120 <recca0120@gamil.com>
2018-06-23 00:18:27 +0800

    測試

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,8 @@
 <?php
 
+use App\Todo;
+use Recca0120\Todolist\Todolist;
+
 /*
 |--------------------------------------------------------------------------
 | Web Routes
@@ -11,6 +14,26 @@
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
+Route::get('/', function (Todolist $todolist) {
+    $todolist->add(new Todo(['text' => 'todo 1']));
+
+    $todos = $todolist->all();
+
+    dump($todos->toArray());
+
+    $todo = $todolist->get($todos->first()->id);
+
+    dump($todo->toArray());
+
+    $todo->fill([
+        'text' => 'todo 2'
+    ])->save();
+
+    dump($todo->toArray());
+
+    dump($todolist->all()->toArray());
+
+    $todolist->delete($todo->id);
+
+    dump($todolist->all()->toArray());
 });
